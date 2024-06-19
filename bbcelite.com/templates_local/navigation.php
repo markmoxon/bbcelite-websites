ccdb14f335f018eac63d718c41cc80e22bb05d56
Mark Moxon <mark@moxon.net>
2024-06-19 16:21:26 +0100

    Added skeleton structure for new bbcelite.com pages

--- bbcelite.com/templates_local/navigation.php
+++ bbcelite.com/templates_local/navigation.php
@@ -19,6 +19,26 @@
 							<li><a id="talks_others" href="/talks/others.html"><span class="menuTitle">Other talks and videos</span> <span class="menuSummary">Talks by other people who have found my research useful</span></a></li>
 						</ul>
 					</li>
+<!--
+					<li id="disassembly_websites"><span class="menuTitle">Creating amazing disassembly websites</span>
+						<span class="menuSummary menuSummarySubmenu">The scripts that generate my source code sites</span>
+						<ul id="submenu_disassembly_websites">
+							<li class="menuItemHeader">Creating amazing disassembly websites</li>
+							<li><a id="disassembly_websites_index" href="/disassembly_websites/"><span class="menuTitle">An overview of my automated scripts</span> <span class="menuSummary">How I automate the creation of my disassembly sites and code repositories</span></a></li>
+							<li><a id="disassembly_websites_generating_elite" href="/disassembly_websites/generating_elite.html"><span class="menuTitle">Generating source code for Elite</span> <span class="menuSummary">Using a library repository to generate and compare seven versions of Elite</span></a></li>
+							<li><a id="disassembly_websites_generating_websites" href="/disassembly_websites/generating_websites.html"><span class="menuTitle">Generating websites from source code</span> <span class="menuSummary">How I create high-quality web disassemblies from hand-crafted code</span></a></li>
+						</ul>
+					</li>
+					<li id="the_art_of_disassembly"><span class="menuTitle">The Art of Disassembly</span>
+						<span class="menuSummary menuSummarySubmenu">How to reconstruct source code from game binaries</span>
+						<ul id="submenu_the_art_of_disassembly">
+							<li class="menuItemHeader">The Art of Disassembly</li>
+							<li><a id="the_art_of_disassembly_index" href="/the_art_of_disassembly/"><span class="menuTitle">The Art of Disassembly</span> <span class="menuSummary">An overview of the disassembly process and source code documentation</span></a></li>
+							<li><a id="the_art_of_disassembly_how_i_disassemble" href="/the_art_of_disassembly/how_i_disassemble."><span class="menuTitle">How I do my disassemblies</span> <span class="menuSummary">A step-by-step guide to disassembling game binaries</span></a></li>
+							<li><a id="the_art_of_disassembly_generating_elite" href="/the_art_of_disassembly/generating_images_of_code.html"><span class="menuTitle">Generating images of code</span> <span class="menuSummary">Visualising game binaries as images</span></a></li>
+						</ul>
+					</li>
+-->
 					<li id="acornsoft"><span class="menuTitle">Acornsoft box screenshots</span>
 						<span class="menuSummary menuSummarySubmenu">Hand-crafted screenshots from Acorn's game boxes</span>
 						<ul id="submenu_acornsoft">
@@ -29,6 +49,18 @@
 							<li><a id="acornsoft_acorn_atom" href="/acornsoft/acorn_atom.html"><span class="menuTitle">Acorn Atom box screenshots</span> <span class="menuSummary">Recreated box screenshots for the Acorn Atom</span></a></li>
 						</ul>
 					</li>
+<!--
+					<li id="retrocomputing_projects"><span class="menuTitle">My other retrocomputing projects</span>
+						<span class="menuSummary menuSummarySubmenu">Some of my ARM projects from back in the day</span>
+						<ul id="submenu_retrocomputing_projects">
+							<li class="menuItemHeader">My other retrocomputing projects</li>
+							<li><a id="retrocomputing_projects_index" href="/retrocomputing_projects/"><span class="menuTitle">An index of my other projects</span> <span class="menuSummary">Details of the projects I wrote back in the day</span></a></li>
+							<li><a id="retrocomputing_projects_jello" href="/retrocomputing_projects/jello.html"><span class="menuTitle">Jello</span> <span class="menuSummary">My version of a Silicon Graphics demo featuring a cube and a jelly icosohedron</span></a></li>
+							<li><a id="retrocomputing_projects_spider" href="/retrocomputing_projects/spider.html"><span class="menuTitle">Spider</span> <span class="menuSummary">A creeping 3D spider that follows the pointer</span></a></li>
+							<li><a id="retrocomputing_projects_isoview" href="/retrocomputing_projects/isoview.html"><span class="menuTitle">IsoView</span> <span class="menuSummary">A 3D isometric game engine</span></a></li>
+						</ul>
+					</li>
+-->
 					<li class="menuItemHeader showForMobile">My software archaeology sites</li>
 					<li class="showForMobile otherSites"><a href="https://www.bbcelite.com"><span class="menuTitle">Mark Moxon's Software Archaeology</span></a></li>
 					<li class="showForMobile otherSites"><a href="https://elite.bbcelite.com"><span class="menuTitle">Elite on the BBC Micro and NES</span></a></li>
