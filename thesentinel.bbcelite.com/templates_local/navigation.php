eac239d660ea31fd7176ddeec82dcf9dc8ba6d90
Mark Moxon <mark@moxon.net>
2026-01-07 13:19:16 +0000

    Add site for The Sentinel

---
+++ thesentinel.bbcelite.com/templates_local/navigation.php
@@ -0,0 +1,75 @@
+			<footer id="copyrightMessage">
+				<p><a title="Copyright information for this website" href="/about_site/acknowledgements.html">The Sentinel, written by Geoff Crammond, &copy; Firebird 1986<br>Commentary &copy; Mark Moxon <span id="copyrightYear">2026</span><br>
+				All Rights Reserved</a></p>
+			</footer>
+
+			<div id="logo">
+				<a href="/">Home page</a>
+			</div>
+
+			<nav id="navigation">
+				<ul class="mainMenu">
+					<li class="menuItemHeader showForMobile">Using this site</li>
+					<li id="home" class="showForMobile"><a href="/"><span class="menuTitle">Home page</span> <span class="menuSummary">Start at the very beginning</span></a></li>
+					<li id="quick_start_guide"><a id="about_site_quick_start_guide" href="/about_site/quick_start_guide.html"><span class="menuTitle">Quick start guide</span> <span class="menuSummary">All you need to know to start exploring this site</span></a></li>
+					<li id="about_site"><span class="menuTitle">All about this project</span>
+						<span class="menuSummary menuSummarySubmenu">Project history, building the source and more</span>
+						<ul id="submenu_about_site">
+							<li class="menuItemHeader">All about this project</li>
+							<li><a id="about_site_about_this_project" href="/about_site/about_this_project.html"><span class="menuTitle">About this project</span> <span class="menuSummary">The story of how this project came to be</span></a></li>
+							<li><a id="about_site_terminology_used_in_this_commentary" href="/about_site/terminology_used_in_this_commentary.html"><span class="menuTitle">Terminology used in this commentary</span> <span class="menuSummary">Read this before you explore the source code</span></a></li>
+							<li><a id="about_site_building_the_sentinel" href="/about_site/building_the_sentinel.html"><span class="menuTitle">Building The Sentinel from the source</span> <span class="menuSummary">How to build your own working copy of The Sentinel</span></a></li>
+							<li><a id="about_site_useful_links" href="/about_site/useful_links.html"><span class="menuTitle">Useful links</span> <span class="menuSummary">A collection of links to pages I've found useful during this project</span></a></li>
+							<li><a id="about_site_acknowledgements" href="/about_site/acknowledgements.html"><span class="menuTitle">Acknowledgements</span> <span class="menuSummary">The giants on whose shoulders this project stands</span></a></li>
+							<li><a id="about_site_site_history" href="/about_site/site_history.html"><span class="menuTitle">Site history</span> <span class="menuSummary">A short history of this site's development</span></a></li>
+							<li><a id="about_site_site_map" href="/about_site/site_map.html"><span class="menuTitle">Site map</span> <span class="menuSummary">A top-level map of this website</span></a></li>
+							<li><a id="about_site_to-do_list" href="/about_site/to-do_list.html"><span class="menuTitle">To-do list</span> <span class="menuSummary">Code that could benefit from a bit more analysis</span></a></li>
+						</ul>
+					</li>
+					<li class="menuItemHeader showForMobile">Playing with The Sentinel source code</li>
+					<li id="playing_the_sentinel"><a id="playing_the_sentinel_index" href="/playing_the_sentinel/"><span class="menuTitle">Playing The Sentinel in the 21st century</span> <span class="menuSummary">How to play all the versions of The Sentinel analysed here</span></a></li>
+
+					<li class="menuItemHeader showForMobile">Exploring The Sentinel source code</li>
+					<li id="source_sources"><span class="menuTitle">The Sentinel source code</span>
+						<span class="menuSummary menuSummarySubmenu">Annotated source of The Sentinel on the BBC Micro</span>
+						<ul id="submenu_source_sources">
+							<li class="menuItemHeader">Version information</li>
+							<li><a id="source_sources_index" href="/source/"><span class="menuTitle">About the version of The Sentinel on this site</span><span class="menuSummary">Details of the different versions of The Sentinel</span></a></li>
+		                    <?php include('navigation_the_sentinel.php'); ?>
+						</ul>
+					</li>
+
+					<li id="random"><a href="/cgi-bin/random.cgi" rel="nofollow"><span class="menuTitle">Show me a Random Routine</span> <span class="menuSummary">Or click &#x221E; above to jump into the source code...</span></a></li>
+
+					<li class="menuItemHeader showForMobile">Indexes and code analyses</li>
+
+					<li id="indexes"><span class="menuTitle">Indexes to the source code</span>
+						<span class="menuSummary menuSummarySubmenu">A-Z indexes, code usage analysis and more</span>
+						<ul id="submenu_source_indexes">
+							<li class="menuItemHeader">A-Z indexes</li>
+							<li><a id="source_indexes_a-z" href="/source/indexes/a-z.html"><span class="menuTitle">A-Z index of the source code</span> <span class="menuSummary">An index of every subroutine, entry point, variable and workspace in the source code</span></a></li>
+							<li><a id="source_indexes_source_code_cross-references" href="/source/articles/source_code_cross-references.html"><span class="menuTitle">Source code cross-references</span> <span class="menuSummary">Every label and variable in the source code and where they are used</span></a></li>
+							<li class="menuItemHeader">Indexes by category</li>
+							<li><a id="source_indexes_subroutines" href="/source/indexes/subroutines.html"><span class="menuTitle">List of all subroutines by category</span> <span class="menuSummary">Subroutines in The Sentinel</span></a></li>
+							<li><a id="source_indexes_variables" href="/source/indexes/variables.html"><span class="menuTitle">List of all variables by category</span> <span class="menuSummary">Variables in The Sentinel</span></a></li>
+							<li><a id="source_indexes_workspaces" href="/source/indexes/workspaces.html"><span class="menuTitle">List of all workspaces by category</span> <span class="menuSummary">Workspaces in The Sentinel</span></a></li>
+						</ul>
+					</li>
+
+					<li id="source_statistics"><a id="source_indexes_source_statistics" href="/source/articles/source_code_statistics.html"><span class="menuTitle">Code statistics for The Sentinel</span> <span class="menuSummary">Instruction counts and other source code stats</span></a></li>
+
+					<li class="menuItemHeader showForMobile">My software archaeology sites</li>
+					<li class="showForMobile otherSites"><a href="https://www.bbcelite.com"><span class="menuTitle">Mark Moxon's Software Archaeology</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://elite.bbcelite.com"><span class="menuTitle">Elite on the 6502</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://aviator.bbcelite.com"><span class="menuTitle">Aviator on the BBC Micro</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://revs.bbcelite.com"><span class="menuTitle">Revs on the BBC Micro</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://thesentinel.bbcelite.com"><span class="menuTitle">The Sentinel on the BBC Micro</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://lander.bbcelite.com"><span class="menuTitle">Lander on the Acorn Archimedes</span></a></li>
+					<li class="menuItemHeader showForMobile">My writing sites</li>
+					<li class="showForMobile otherSites"><a href="https://www.moxon.net"><span class="menuTitle">Mark Moxon's Travel Writing</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://www.landsendjohnogroats.info"><span class="menuTitle">Walking Land's End to John o'Groats</span></a></li>
+					<li class="showForMobile otherSites"><a href="https://www.tubewalker.com"><span class="menuTitle">Tubewalker: The Tube, on Foot</span></a></li>
+					<li class="menuItemHeader showForMobile">Contact details and more</li>
+					<li class="showForMobile otherSites"><a href="https://www.markmoxon.com"><span class="menuTitle">Mark Moxon's Homepage</span></a></li>
+				</ul>
+			</nav>
